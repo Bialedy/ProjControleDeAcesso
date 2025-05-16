87db736841ccac34da4db62f22982a4afd0ac772
bianca Alves <biaalves4589@gmail.com>
2025-05-16 10:40:37 -0300

    teste

--- src/main/java/com/Senai/Teste_Brenches/teste.java
+++ src/main/java/com/Senai/Teste_Brenches/teste.java
@@ -3,5 +3,6 @@ package com.Senai.Teste_Brenches;
 public class teste {
     public static void main(String[] args) {
         System.out.println("primeiro teste com o grupo");
+        System.out.println("oi");
     }
 }
